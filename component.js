7537d34428da5436ca2c2a89b463b34f5ea72f0b
Tanveer Najib <tanveernajib@gmail.com>
2025-06-26 12:15:58 +0100

    Add collides with

--- component.js
+++ component.js
@@ -17,4 +17,13 @@ class Component {
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
   }
+  
+  collidesWith(otherComponent) {
+    return (
+      this.position.x + this.width >= otherComponent.position.x && // box1 right collides with box2 left
+      otherComponent.position.x + otherComponent.width >= this.position.x && // box2 right collides with box1 left
+      this.position.y + this.height >= otherComponent.position.y && // box1 bottom collides with box2 top
+      otherComponent.position.y + otherComponent.height >= this.position.y // box1 top collides with box2 bottom
+    )
+  }
 }

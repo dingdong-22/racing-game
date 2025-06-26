b8c2622e5dacc1e4f4fbb9e4f8be3c2930c27b72
Alex Deng <alex_deng22@outlook.com>
2025-06-26 12:25:29 +0100

    Tweak Car.js

--- car.js
+++ car.js
@@ -1,6 +1,6 @@
 class Car extends Component {
   constructor({ctx, position = { x: 0, y: 0 }, color = 'red', width = 100, height = 100, velocity = { x: 0, y: 0 }}) {
-    super({ctx, position: { x: 0, y: 0 }, color: 'red', width: 100, height: 100, velocity: { x: 0, y: 0 }})
+    super({ctx, position, color, width, height, velocity})
     this.img = new Image();
     this.img.src = "assets/Cars/car_black_small_1.png";
   }
